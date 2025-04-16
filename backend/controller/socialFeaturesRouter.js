0d92f9d5de561cab794037e8c482772ca0989177
Rekhansika <rekhagoluguri02@gmail.com>
2025-04-16 17:33:43 +0530

    post

--- backend/controller/socialFeaturesRouter.js
+++ backend/controller/socialFeaturesRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const socialFeaturesRouter = express.Router();
 
+const {communityPost,post} = require("../models/communityPostSchema");
+
 
 socialFeaturesRouter.get('/communities', async (req, res) => {
     try {
@@ -11,5 +13,30 @@ socialFeaturesRouter.get('/communities', async (req, res) => {
         res.status(500).send({ message: "Something went wrong" });
     }
 });
+
+socialFeaturesRouter.post('/communityposts',async (req, res) => {
+    try {
+      const { communityName, userName, title, content } = req.body;
+      const newCommunityPost = new communityPost({ communityName, userName, title, content });
+      await newCommunityPost.save();
+      res.status(201).send({ message: 'Community post created successfully!', post: newCommunityPost });
+    } catch (error) {
+      res.status(500).send({ message: 'Error creating community post', error });
+      console.log(error);
+    }
+  });
+  
+  
+socialFeaturesRouter.post('/posts',async (req, res) => {
+    try {
+      const { userName, title, content, tags } = req.body;
+      const newPost = new post({ userName, title, content, tags });
+      await newPost.save();
+      res.status(201).send({ message: 'Post created successfully!', post: newPost });
+    } catch (error) {
+      res.status(500).send({ message: 'Error creating post', error });
+    }
+  });
+  
 
 module.exports = socialFeaturesRouter;

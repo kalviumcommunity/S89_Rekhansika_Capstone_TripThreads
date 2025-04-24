6845e147d3c647f4c07ee2502a3da766f27ca21c
Rekhansika <rekhagoluguri02@gmail.com>
2025-04-24 10:34:15 +0530

    schema

--- backend/models/navigationPreferenceSchema.js
+++ backend/models/navigationPreferenceSchema.js
@@ -1,27 +1,59 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const navigationPreferenceSchema = new mongoose.Schema({
-    userName: {
+const transportationSchema = new mongoose.Schema({
+    mode_of_transportation: {
         type: String,
+        required: true
+    },
+    start_date: {
+        type: Date,
+        required: true
+    },
+    end_date: {
+        type: Date,
         required: true,
-        ref: 'User'
+        validate: {
+            validator: function (value) {
+                return value > this.start_date;
+            },
+            message: "End date must be after start date"
+        }
     },
-    preferredRoutes: {
-        type: [String], 
-        default: []
-    },
-    transportOptions: {
-        type: [String], 
-        default: []
-    },
-    mapDirections: {
-        type: String, 
-        required: true,
-        trim: true
+    location: {
+        type: String,
+        required: true
     }
-},{
-    timestamps:true
 });
 
-const navigationPreference = mongoose.model('NavigationPreference', navigationPreferenceSchema);
-module.exports = navigationPreference;
+const hotelSchema = new mongoose.Schema({
+    type_of_hotel: {
+        type: String,
+        required: true
+    },
+    start_date: {
+        type: Date,
+        required: true
+    },
+    end_date: {
+        type: Date,
+        required: true,
+        validate: {
+            validator: function (value) {
+                return value > this.start_date;
+            },
+            message: "End date must be after start date"
+        }
+    },
+    location: {
+        type: String,
+        required: true
+    }
+});
+
+const bookingsSchema = new mongoose.Schema({
+    transportation: transportationSchema,
+    hotels: [hotelSchema]
+});
+
+const bookings = mongoose.model("bookings", bookingsSchema);
+module.exports = bookings;

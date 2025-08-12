de2183dfca4e743edcb32344edc842f75cb79f7d
Rekhansika <rekhagoluguri02@gmail.com>
2025-08-12 15:36:38 +0530

    Deployment

--- frontend/src/components/UserPosts.jsx
+++ frontend/src/components/UserPosts.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams} from "react-router-dom";
-import "./UserPosts.css";
+import "./Userposts.css";
 import { FaMapMarkerAlt } from "react-icons/fa";
 
 const UserPosts = () => {
